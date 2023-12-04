e2076303c3fe58c4ce2f27c496922541f39f5558
Antsa-Fi30 <antsafiononana30@gmail.com>
2023-12-04 16:16:59 +0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        bluePrimary: "#49DEFF",
+        tealPrimary: "#4FFFDF",
+        blackPrimary: "#303030",
+        secondary01: "#4E4E4E",
+        secondary02: "#DEDEDE",
+        secondary03: "#F4F4F4",
+        secondary04: "#F9F9F9",
+        textInDark: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
