d4e0aac33a6299ca15c5301ce0121859ef7799ec
Antsa-Fi30 <antsafiononana30@gmail.com>
2023-12-09 13:53:53 +0300

    Header and conception of heading, responsivity not very stable

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,9 @@ module.exports = {
     },
     extend: {
       colors: {
-        bluePrimary: "#49DEFF",
-        tealPrimary: "#4FFFDF",
-        blackPrimary: "#303030",
-        secondary01: "#4E4E4E",
-        secondary02: "#DEDEDE",
-        secondary03: "#F4F4F4",
-        secondary04: "#F9F9F9",
-        textInDark: "#FFFFFF",
+        blueColor: "#1B3764",
+        yellowColor: "#FFCA42",
+        whiteColor: "#F6F8FC",
       },
     },
   },
